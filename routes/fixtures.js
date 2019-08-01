122b4daabae00c52d05c413841c663d006bb1a03
netaro-N <takebbb1+github@gmail.com>
2019-08-01 01:13:06 +0000

    match page && render

--- routes/fixtures.js
+++ routes/fixtures.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('fixtures', { user:req.user });
 });
 
+router.get('/:fixtureId', function(req, res, next) {
+  //　ここに、個別ページへの実装をする
+  res.render('match', { 
+    user:req.user,
+    ID:req.params.fixtureId
+  })
+})
+
 module.exports = router;
