0418a3926a654b991a7ca578c7bf8408fc65ab4c
netaro-N <takebbb1+github@gmail.com>
2019-08-18 23:31:07 +0000

    POST new

--- routes/manage.js
+++ routes/manage.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const moment = require('moment-timezone');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
+const Fixture = require('../models/fixture');
 const csvParse = require('csv-parse/lib/sync'); // requiring sync module 同期でパースする
 
 /* GET manage page. */
@@ -31,8 +32,26 @@ router.get('/:fixtureId', function(req, res, next) {
 /* POST 新規作成 */
 router.post('/new', (req, res, next) => {
   // ここにデータベースへ保存実装
-
-})
+  Fixture.findAll({
+    where: {
+      fixtureId: { [Op.like]: req.body.fixtureId + '%' } // fixtureDate <= japanTimeplus2
+    }
+  }).then((id) => {
+    //fixtureId,fixtureDate,fixtureSort,homeTeam,awayTeam,homeScore,awayScore
+    const fixtureDate = new Date(req.body.fixtureDate);
+    const formattedDate = moment(fixtureDate).format("YYYY/MM/DD HH:mm");
+    Fixture.upsert({
+      fixtureId: req.body.fixtureId + (id.length + 1),
+      fixtureDate: formattedDate,
+      fixtureSort: req.body.fixtureSort,
+      homeTeam: req.body.homeTeam,
+      awayTeam: req.body.awayTeam,
+      homeScore: '',
+      awayScore: ''
+    });
+    res.redirect('/new');
+  });
+});
 
 /* POST csv file */
 router.post('/insert', (req, res, next) => {
