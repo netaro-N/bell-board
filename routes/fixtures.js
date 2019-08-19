57389bac13ca0c374229954a75b309be34425436
netaro-N <takebbb1+github@gmail.com>
2019-08-19 01:17:07 +0000

    GET fixtures

--- routes/fixtures.js
+++ routes/fixtures.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment-timezone');
+const Fixture = require('../models/fixture')
 
 /* GET fixtures page. */
 router.get('/', function(req, res, next) {
-  res.render('fixtures', { user:req.user });
+  Fixture.findAll({
+    order: [['"fixtureDate"', 'ASC']]
+  }).then((fixtures) => {
+    if (fixtures) {
+      fixtures.forEach((f) => {
+        f.formattedDate = moment(f.fixtureDate).format('YYYY/MM/DD (ddd) HH:mm');
+      });
+      res.render('fixtures', {
+        title: '試合一覧ページ',
+        fixtures: fixtures
+      });
+    } else {
+      const err = new Error('試合一覧がございません。申し訳ございません。');
+      err.status = 404;
+      next(err);
+    };
+  });
 });
 
 /* GET 個別ページ */
